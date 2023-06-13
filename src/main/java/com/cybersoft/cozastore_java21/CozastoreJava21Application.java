880226fb1704660d27402950ed2a55d6d0984faa
danhNg.8 <105102066+danhNg08101997@users.noreply.github.com>
2023-06-13 18:09:24 +0700

    first commit

---
+++ src/main/java/com/cybersoft/cozastore_java21/CozastoreJava21Application.java
@@ -0,0 +1,13 @@
+package com.cybersoft.cozastore_java21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CozastoreJava21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CozastoreJava21Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cybersoft/cozastore_java21/CozastoreJava21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cybersoft.cozastore_java21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CozastoreJava21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
